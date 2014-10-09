644dca50413404c50b7d26dac67b1cdbf7155043
anne55n <anne55n@yahoo.com>
2014-10-09 10:17:00 -0500

    Update portfolio.js

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,4 +1,16 @@
-    document.getElementById("btn").onclick = function () {    location.href = "https://twitter.com/Violettycatty";    };     document.getElementById("btn1").onclick =function() {      location.href = "https://www.linkedin.com/profile/view?id=250007829&trk=nav_responsive_tab_profile_pic";    };    document.getElementById("btn2").onclick =function() {      location.href = "https://www.facebook.com/manonLesaut";    };     document.getElementById("btn3").onclick =function() {      location.href = "https://github.com/anne55n";    };
+    document.getElementById("btn").onclick = function () {
+    location.href = "https://twitter.com/your_twitter_profile_account";
+    };
+     document.getElementById("btn1").onclick =function() {
+      location.href = "https://www.linkedin.com/your_linkedin_profile_account";
+    };
+    document.getElementById("btn2").onclick =function() {
+      location.href = "https://www.facebook.com/your_facebook_account";
+    };
+     document.getElementById("btn3").onclick =function() {
+      location.href = "https://github.com/your_github_account_profile_name";
+    };
+
       document.getElementById("btn4").onclick =function() {
       location.href = "http://localhost/arop/aboutMe.html";
     };
