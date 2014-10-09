c0b0f49e32c5e0a08da206908515153e60e706f5
Anastassiya Neznanova <anne55n@yahoo.com>
2014-10-09 10:59:29 -0500

    The main page
    Portfolio

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,13 @@
+    document.getElementById("btn").onclick = function () {    location.href = "https://twitter.com/Violettycatty";    };     document.getElementById("btn1").onclick =function() {      location.href = "https://www.linkedin.com/profile/view?id=250007829&trk=nav_responsive_tab_profile_pic";    };    document.getElementById("btn2").onclick =function() {      location.href = "https://www.facebook.com/manonLesaut";    };     document.getElementById("btn3").onclick =function() {      location.href = "https://github.com/anne55n";    };
+      document.getElementById("btn4").onclick =function() {
+      location.href = "http://localhost/arop/aboutMe.html";
+    };
+      document.getElementById("btn5").onclick =function() {
+      location.href = "http://localhost/arop/butn1.html";
+    };
+    document.getElementById("btn6").onclick =function() {
+      location.href = "http://localhost/arop/project.html";
+    };
+     document.getElementById("btn7").onclick =function() {
+      location.href = "http://localhost/instaThing/index.html";
+    };
